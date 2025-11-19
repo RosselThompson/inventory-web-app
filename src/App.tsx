e578a10c1458c44e61754f391a090cc75c8ccb1a
Rossel Thompson <exrthom@bci.cl>
2025-11-19 12:04:13 -0600

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h1>Vite + React</h1>
+			<Button onClick={() => setCount((count) => count + 1)}>
+				count is {count}
+			</Button>
+		</>
+	);
+}
+
+export default App;
